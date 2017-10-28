6ff117935f5315ff3f021a5af5e3c890bb81a2f7
SD <namelosted@gmail.com>
2017-10-28 18:33:32 +0300

    add new element 'label'

---
+++ src/iface/html/label.h
@@ -0,0 +1,18 @@
+#ifndef G18856288_D63B_4F89_B1AD_B422169C31F2
+#define G18856288_D63B_4F89_B1AD_B422169C31F2
+
+
+#include <html/details/symetric_tag_with_attributes.h>
+
+
+namespace html
+{
+class Label final : public details::SymetricTagWithAttributes
+{
+public:
+	Label(): SymetricTagWithAttributes(L"label") {}
+};
+}
+
+
+#endif//G18856288_D63B_4F89_B1AD_B422169C31F2
